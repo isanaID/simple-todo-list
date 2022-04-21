a48c88120a0b476b3f2ecf239bd9273bcab7cb5e
isanaid <eizisana@gmail.com>
2022-04-21 22:58:32 +0700

    delete task

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/index';
 import * as React from 'react';
 import data from './data.json';
 import ToDoList from './components/ToDoList';
+import ToDoForm from './components/ToDoForm';
 
 function App() {
   const [ toDoList, setToDoList ] = React.useState(data);
 
+  const handleToggle = (id) => {
+    let mapped = toDoList.map(task => {
+      return task.id === id ? {...task, complete: !task.complete} : {...task};
+    });
+    setToDoList(mapped);
+  }
+
+  const handleFilter = () => {
+    let filtered = toDoList.filter(task => {
+      return !task.complete;
+    });
+    setToDoList(filtered);
+  }
+
+  const addTask = (userInput) => {
+    let copy = [...toDoList];
+    copy = [...copy, {id: toDoList.length + 1, task: userInput, complete: false}];
+    setToDoList(copy);
+  }
+
   return (
     <div className="App">
       <Header />
-      <ToDoList toDoList={toDoList} />
+      <ToDoList toDoList={toDoList} handleToggle={handleToggle} handleFilter={handleFilter}/>
+      <ToDoForm addTask={addTask}/>
     </div>
   );
 }
